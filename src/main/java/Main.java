3173a410a4742aec4dad377398b6193fa11adfee
ruslanv-r <rvregi@gmail.com>
2022-04-17 22:10:14 +0900

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        for (int r : new Randoms(90, 100)) {
+        for (int r : new Randoms(80, 100)) {
             System.out.println("Случайное число: " + r);
             if (r == 100) {
                 System.out.println("Выпало число 100, давайте на этом закончим");
--- src/main/java/Randoms.java
+++ src/main/java/Randoms.java
@@ -2,16 +2,31 @@ import java.util.Iterator;
 import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
-    protected Random random;
+   protected Random random;
+    protected int min;
+    protected int max;
+
+
     public Randoms(int min, int max) {
+        this.random=new Random();
         this.min=min;
-        this.maa=max;
-
-
-    }
+        this.max=max;
+        }
 
     @Override
     public Iterator<Integer> iterator() {
-        return null;
+        return new Iterator<Integer>() {
+
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+
+                return (int) (Math.random() * ((max - min) + 1)) + min;
+            }
+        };
     }
 }
